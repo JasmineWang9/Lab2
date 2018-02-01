fe02b06db709be6fe8adc000045b7a947cd7e89f
Jasmine.W <jw9@illinois.edu>
2018-01-31 18:38:47 -0600

    finished

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,22 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while(!maze.isFinished()) {
+                if (maze.canMove()) {
+                    maze.move();
+                }
+                if (!maze.canMove()) {
+                    maze.turnLeft();
+                    if (!maze.canMove()) {
+                        maze.turnRight();
+                        maze.turnRight();
+                        if (!maze.canMove()) {
+                            maze.turnRight();
+                        }
+                    }
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
